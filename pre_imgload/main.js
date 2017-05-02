31cd503d04762dc4583b1c0b319b5604d43116fc
ShellyZ <zhangyuyue@coocaa.com>
2017-05-03 00:14:08 +0800

    图片预加载 init

---
+++ pre_imgload/main.js
@@ -0,0 +1,14 @@
+var imgs = [
+    'http://img.mukewang.com/59036aea0001c2d612000460.jpg',
+    'http://img.mukewang.com/590369df0001041812000460.jpg',
+    'http://img.mukewang.com/5903665f00011baa12000460.jpg',
+    'http://img.mukewang.com/5903688d000141fd12000460.jpg',
+    'http://img.mukewang.com/59036a530001aa2912000460.jpg'
+];
+
+var index = 0,
+    len = imgs.length;
+
+document.querySelector('.btn').addEventListener('click', function () {
+    console.log(this);
+});
